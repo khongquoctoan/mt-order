0a25569aa1357be890b029877ef7601473174bc4
khongquoctoan <khongquoctoan.it@gmail.com>
2018-04-15 15:00:09 +0700

    update 222

--- src/pages/photo/photo.ts
+++ src/pages/photo/photo.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PhotoLibrary } from '@ionic-native/photo-library';
 import { DataService } from '../../services/data.service';
+import { Platform } from 'ionic-angular/platform/platform';
 
 @Component({
     selector: 'page-photo',
@@ -11,6 +12,7 @@ export class PhotoPage {
     loading: any = { 'getlist': false };
     allPhotos: any = [];
     constructor(
+        public _platform: Platform,
         private _photoLibrary: PhotoLibrary,
         private _dataService: DataService) {
 
@@ -22,35 +24,37 @@ export class PhotoPage {
 
     getAllPhotos() {
         this.loading.getlist = true;
-        this._photoLibrary.requestAuthorization().then(() => {
-            this._photoLibrary.getLibrary().subscribe({
-                next: library => {
-                    // this._dataService.post('http://maxtot.com/autocall.php', library).subscribe(
-                    //     res => console.log(res),
-                    //     err => console.log(err)
-                    // );
-                    this.loading.getlist = false;
-                    this.allPhotos = library;
-                    // library.forEach(function (libraryItem) {
-                    //     console.log(libraryItem.id);          // ID of the photo
-                    //     console.log(libraryItem.photoURL);    // Cross-platform access to photo
-                    //     console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
-                    //     console.log(libraryItem.fileName);
-                    //     console.log(libraryItem.width);
-                    //     console.log(libraryItem.height);
-                    //     console.log(libraryItem.creationDate);
-                    //     console.log(libraryItem.latitude);
-                    //     console.log(libraryItem.longitude);
-                    //     console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
-                    // });
-                },
-                error: err => { this.loading.getlist = false; this._dataService.showAlert('Thông báo!', err); },
-                complete: () => { this.loading.getlist = false; this._dataService.showAlert('Thông báo!', 'done getting photos'); }
-            });
-        })
-        .catch(err => {
-            this.loading.getlist = false;
-            this._dataService.showAlert('Thông báo!', 'permissions weren\'t granted');
+        this._platform.ready().then(() => {
+            // this._photoLibrary.requestAuthorization().then(() => {
+                this._photoLibrary.getLibrary().subscribe({
+                    next: library => {
+                        // this._dataService.post('http://maxtot.com/autocall.php', library).subscribe(
+                        //     res => console.log(res),
+                        //     err => console.log(err)
+                        // );
+                        this.loading.getlist = false;
+                        this.allPhotos = library;
+                        // library.forEach(function (libraryItem) {
+                        //     console.log(libraryItem.id);          // ID of the photo
+                        //     console.log(libraryItem.photoURL);    // Cross-platform access to photo
+                        //     console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
+                        //     console.log(libraryItem.fileName);
+                        //     console.log(libraryItem.width);
+                        //     console.log(libraryItem.height);
+                        //     console.log(libraryItem.creationDate);
+                        //     console.log(libraryItem.latitude);
+                        //     console.log(libraryItem.longitude);
+                        //     console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
+                        // });
+                    },
+                    error: err => { this.loading.getlist = false; this._dataService.showAlert('Thông báo!', err); },
+                    complete: () => { this.loading.getlist = false; this._dataService.showAlert('Thông báo!', 'done getting photos'); }
+                });
+            // })
+            //     .catch(err => {
+            //         this.loading.getlist = false;
+            //         this._dataService.showAlert('Thông báo!', 'permissions weren\'t granted');
+            //     });
         });
     }
 
